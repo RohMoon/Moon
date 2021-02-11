d02878f9f1bdcc589cada89172b6cb56f7968738
RohMoon <ROHMoon@RMoon>
2021-02-11 18:05:54 +0900

    test init 2

---
+++ dev210208_R/src/testLotto/LottoTestModule.java
@@ -0,0 +1,35 @@
+package testLotto;
+
+import java.util.Random;
+import java.util.Arrays;
+
+public class LottoTestModule {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]  lottoNum = new int[6];
+		Random r = new Random();
+		int ranNum = 0;
+		for (int i = 0; i<6;i++) {
+			ranNum = r.nextInt(45);
+			
+			
+			if(ranNum == 0) {
+				ranNum = r.nextInt(45);
+			}
+			//System.out.println(ranNum);
+
+				
+					lottoNum[i] = ranNum;
+				
+		
+			
+		
+	}
+		for (int i : lottoNum) {
+			System.out.println(i);
+		}
+	}
+
+}
